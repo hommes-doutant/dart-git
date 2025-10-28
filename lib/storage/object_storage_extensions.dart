a452a2946cc696c58361b9e54291e82e0a3c0ccf
hommes-doutant <thomas.dumonet@gmail.com>
2025-10-28 14:36:05 +0100

    fix

--- lib/storage/object_storage_extensions.dart
+++ lib/storage/object_storage_extensions.dart
@@ -1,45 +1,64 @@
-import 'package:path/path.dart' as p;
+// lib/storage/object_storage_extensions.dart
 
 import 'package:dart_git/exceptions.dart';
 import 'package:dart_git/plumbing/git_hash.dart';
 import 'package:dart_git/plumbing/objects/blob.dart';
 import 'package:dart_git/plumbing/objects/commit.dart';
 import 'package:dart_git/plumbing/objects/tree.dart';
-
 import 'package:dart_git/utils/utils.dart';
 import 'interfaces.dart';
 
 extension ObjectStorageExtension on ObjectStorage {
-  GitTreeEntry refSpec(GitTree tree, String spec) {
-    assert(!spec.startsWith(p.separator));
-
-    if (spec.isEmpty) {
+  /// Resolves a pathspec (e.g., "path/to/file.txt") against a root tree.
+  ///
+  /// This method traverses the tree structure to find the [GitTreeEntry]
+  /// corresponding to the given path.
+  ///
+  /// Throws [GitObjectWithRefSpecNotFound] if the path does not exist.
+  Future<GitTreeEntry> refSpec(GitTree tree, String spec) async {
+    if (spec.isEmpty || spec.startsWith('/')) {
       throw GitObjectWithRefSpecNotFound(spec);
     }
 
-    var parts = splitPath(spec);
-    var name = parts.item1;
-    var remainingName = parts.item2;
+    final parts = splitPath(spec);
+    final name = parts.item1;
+    final remainingName = parts.item2;
 
-    for (var leaf in tree.entries) {
+    for (final leaf in tree.entries) {
       if (leaf.name == name) {
         if (remainingName.isEmpty) {
+          // Found the final entry in the path.
           return leaf;
         }
 
-        var obj = read(leaf.hash);
+        // The path continues, so this entry must be a tree.
+        final obj = await read(leaf.hash);
         if (obj is GitTree) {
-          return refSpec(obj, remainingName);
+          // Recurse into the sub-tree with the rest of the path.
+          return await refSpec(obj, remainingName);
         }
 
+        // Path spec implies a directory, but this is a blob.
         throw GitObjectWithRefSpecNotFound(spec);
       }
     }
+
+    // No entry matched the current part of the path.
     throw GitObjectWithRefSpecNotFound(spec);
   }
 
-  // TODO: What happens when we call readBlob on a commit?
-  GitBlob readBlob(GitHash hash) => read(hash) as GitBlob;
-  GitTree readTree(GitHash hash) => read(hash) as GitTree;
-  GitCommit readCommit(GitHash hash) => read(hash) as GitCommit;
+  /// Convenience method to read an object and cast it to a [GitBlob].
+  ///
+  /// Throws a [TypeError] if the object is not a blob.
+  Future<GitBlob> readBlob(GitHash hash) async => await read(hash) as GitBlob;
+
+  /// Convenience method to read an object and cast it to a [GitTree].
+  ///
+  /// Throws a [TypeError] if the object is not a tree.
+  Future<GitTree> readTree(GitHash hash) async => await read(hash) as GitTree;
+
+  /// Convenience method to read an object and cast it to a [GitCommit].
+  ///
+  /// Throws a [TypeError] if the object is not a commit.
+  Future<GitCommit> readCommit(GitHash hash) async => await read(hash) as GitCommit;
 }
