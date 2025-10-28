f09b702c30e5ad072798848650b221839d6d3b6b
hommes-doutant <thomas.dumonet@gmail.com>
2025-10-28 19:46:33 +0100

    fixed some tests

--- test/async_repo_test.dart
+++ test/async_repo_test.dart
@@ -1,8 +1,10 @@
+// FILE: test/async_repo_test.dart
 import 'dart:io';
 
 import 'package:test/test.dart';
 
-import 'package:dart_git/git_async.dart';
+import 'package:dart_git/dart_git.dart';
+import 'package:dart_git/exceptions.dart';
 import 'package:dart_git/plumbing/git_hash.dart';
 
 import 'lib.dart';
@@ -16,15 +18,20 @@ void main() {
   });
 
   test('Basic', () async {
-    var repo = await GitAsyncRepository.load(gitDir);
+    // Change 1: Use the main GitRepository.local() factory instead of GitAsyncRepository.load()
+    var repo = await GitRepository.local(gitDir);
+
     expect(await repo.currentBranch(), "master");
     expect(await repo.branches(), ["master"]);
     expect(await repo.headHash(),
         GitHash('386de870a014e32234ce7f87e59a1beb06f720df'));
+
+    // Change 2: Assert a more specific exception type for better testing.
     await expectLater(() async {
       await repo.deleteBranch('non-existent-branch');
-    }, throwsA(isA<Exception>()));
+    }, throwsA(isA<GitRefNotFound>()));
 
-    repo.close();
+    // Change 3: The close() method is no longer part of the public GitRepository API.
+    // repo.close();
   });
 }
